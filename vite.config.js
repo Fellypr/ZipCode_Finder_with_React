c17aace8ec444e63d6b21d1fc0516cd81d03bced
fellypr <fellype29kennedemil@gmail.com>
2025-06-30 17:50:27 -0300

    Base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ZipCode_Finder_with_React/',
+  base: '/',
   plugins: [react()],
 })
