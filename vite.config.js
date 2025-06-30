52e659a191a36be30eb50eaaf23cd3805a65da41
fellypr <fellype29kennedemil@gmail.com>
2025-06-30 17:49:04 -0300

    Mudando Background

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/ZipCode_Finder_with_React/',
   plugins: [react()],
 })
